b957888a7a7bd0417457a0e7a1be3c9e753b2f08
Yevheniia Velychko <y.velychko@UAIEV8862NBL.levi9.com>
2019-08-12 16:48:45 +0300

    logic in service

---
+++ src/app/movement.service.ts
@@ -0,0 +1,390 @@
+// import {Injectable} from '@angular/core';
+// import {BehaviorSubject, combineLatest, fromEvent, interval, merge, Observable, Subject, Subscription} from 'rxjs';
+// import {distinctUntilChanged, filter, map, mapTo, scan, startWith, switchMap, takeUntil, tap, withLatestFrom} from 'rxjs/operators';
+// import {DIRECTIONS, Key, SNAKE_LENGTH, SNAKE_START} from './snake/snake.component';
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MovementService {
+//   score$ = new BehaviorSubject(0);
+//   left$: Observable<any> = fromEvent(document, 'keydown').pipe(
+//     filter((event: KeyboardEvent) => event.keyCode === Key.LEFT)
+//   );
+//   up$: Observable<any> = fromEvent(document, 'keydown').pipe(
+//     filter((event: KeyboardEvent) => event.keyCode === Key.UP)
+//   );
+//
+//   down$: Observable<any> = fromEvent(document, 'keydown').pipe(
+//     filter((event: KeyboardEvent) => event.keyCode === Key.DOWN)
+//   );
+//
+//   right$: Observable<any> = fromEvent(document, 'keydown').pipe(
+//     filter((event: KeyboardEvent) => event.keyCode === Key.RIGHT)
+//   );
+//   snakeMovement$: Observable<{ key: number; direction: { x: number; y: number } }> = merge(
+//     this.left$.pipe(mapTo({key: Key.LEFT, direction: DIRECTIONS[37]})),
+//     this.up$.pipe(mapTo({key: Key.UP, direction: DIRECTIONS[38]})),
+//     this.down$.pipe(mapTo({key: Key.DOWN, direction: DIRECTIONS[40]})),
+//     this.right$.pipe(mapTo({key: Key.RIGHT, direction: DIRECTIONS[39]})),
+//   ).pipe(
+//     startWith({key: 39, direction: DIRECTIONS[39]}),
+//     scan((acc, current) => {
+//       if (Math.abs(acc.key - current.key) === 2) {
+//         return acc;
+//       } else {
+//         return current;
+//       }
+//     }),
+//     distinctUntilChanged(),
+//   );
+//
+//   // startGame(document) {
+//   //
+//   //   this.left$ = this.getKeydownLeft(document, Key.LEFT);
+//   //
+//   //
+//   //   return this.game$;
+//   // }
+//
+//   // getKeydownLeft(doc, key) {
+//   //   return fromEvent(doc, 'keydown').pipe(
+//   //     filter((event: KeyboardEvent) => event.keyCode === key)
+//   //   );
+//   // }
+//
+//   game: Subscription;
+//   snake: Array<any> = [];
+//   xSide: number;
+//   ySide: number;
+//   ticks$ = interval(1000);
+//
+//   apples$ = new BehaviorSubject([]);
+//   snake$ = new Subject();
+//
+//   startGame$ = new Subject();
+//
+//   snake2$ = this.startGame$.pipe(
+//     switchMap((data: {
+//       snake: {
+//         length: number;
+//         startPoint: {}
+//       }
+//     }) => this.printSnakeAtTheBeggining(data.snake.length)));
+//
+//   move$ = this.ticks$.pipe(
+//     withLatestFrom(this.snake2$, this.snakeMovement$, this.apples$),
+//     map(([tick, snakeArr, movement, apples]) => {
+//       this.moveSnake(tick, snakeArr, movement.direction, apples);
+//     })
+//   );
+//
+//   game$ = combineLatest(
+//     this.move$,
+//     this.apples$,
+//     this.snake2$,
+//     this.ticks$,
+//     this.snakeMovement$
+//   ).pipe(
+//     tap((data) => console.log(data))
+//     // takeUntil(this.move$)
+//   );
+//
+//   startGame2(x: string, y: string) {
+//     this.xSide = +x;
+//     this.ySide = +y;
+//
+//     if (this.game) {
+//       this.unsubscribe();
+//     }
+//
+//     this.game = this.game$
+//       .subscribe(() => this.startGame$.next({
+//         snake: {
+//           length: SNAKE_LENGTH,
+//           startPoint: SNAKE_START
+//         },
+//       }));
+//
+//     this.generateApple();
+//     this.generateApple();
+//   }
+//
+//   unsubscribe() {
+//     this.game.unsubscribe();
+//     this.score$.next(0);
+//
+//     // this.snake.forEach(cell => this.rows[cell.x].children[cell.y].classList.remove('snake'));
+//     // this.apples$.getValue().forEach(cell => this.rows[cell.x].children[cell.y].classList.remove('apple'));
+//     this.apples$.next([]);
+//
+//     const elem = document.getElementsByClassName('table')[0];
+//     elem.parentNode.removeChild(elem);
+//
+//     this.startGame$.next({
+//       snake: {
+//         length: SNAKE_LENGTH,
+//         startPoint: SNAKE_START
+//       },
+//     });
+//   }
+//
+//   printSnakeAtTheBeggining(length) {
+//     this.snake = [];
+//     for (let i = 0; i < length; i++) {
+//       this.snake.push({x: 0, y: i});
+//      // this.rows[0].children[i].classList.add('snake');
+//     }
+//
+//     this.snake$.next(this.snake);
+//     return this.snake$;
+//   }
+//
+//   generateApple() {
+//     let i = Math.floor(Math.random() * ((this.xSide - 1) + 1));
+//     const j = Math.floor(Math.random() * ((this.ySide - 1) + 1));
+//
+//     // while (this.rows[i].children[j].className === 'cell snake') {
+//     //   i = Math.floor(Math.random() * ((this.xSide - 1) + 1));
+//     // }
+//
+//     this.apples$.getValue().forEach(apple => {
+//       if (apple.x === i && apple.y === j) {
+//         i = Math.floor(Math.random() * ((this.ySide - 1) + 1));
+//       }
+//     });
+//     //this.rows[i].children[j].classList.add('apple');
+//     this.apples$.getValue().push({x: i, y: j});
+//   }
+//
+//   moveSnake(tick, snakeArr, direction, apples) {
+//     //snakeArr.forEach(cell => this.rows[cell.x].children[cell.y].classList.add('snake'));
+//     let yNext, xNext;
+//
+//     yNext = snakeArr[snakeArr.length - 1].y;
+//
+//     xNext = snakeArr[snakeArr.length - 1].x;
+//
+//     xNext += direction.x;
+//     yNext += direction.y;
+//
+//     if (yNext > this.ySide - 1) {
+//       yNext = 0;
+//     }
+//
+//     if (yNext < 0) {
+//       yNext = this.ySide - 1;
+//     }
+//
+//     if (xNext < 0) {
+//       xNext = this.xSide - 1;
+//     }
+//
+//     if (xNext > this.xSide - 1) {
+//       xNext = 0;
+//     }
+//
+//     this.gameOver(snakeArr, xNext, yNext);
+//
+//     //this.rows[xNext].children[yNext].classList.add('snake');
+//
+//     snakeArr.push({x: xNext, y: yNext});
+//
+//     let newSnakeCell = false;
+//     snakeArr.forEach(snakeCoord => {
+//       apples.forEach((apple, index) => {
+//         if (apple.x === snakeCoord.x && apple.y === snakeCoord.y) {
+//           //this.rows[apples[index].x].children[apples[index].y].classList.remove('apple');
+//           apples.splice(index, 1);
+//
+//           newSnakeCell = true;
+//         }
+//       });
+//     });
+//
+//     if (newSnakeCell) {
+//       this.generateApple();
+//       this.score$.next(this.score$.getValue() + 1);
+//     }
+//
+//     if (!newSnakeCell) {
+//      // this.rows[snakeArr[0].x].children[snakeArr[0].y].classList.remove('snake');
+//       snakeArr.shift();
+//     }
+//
+//     return snakeArr;
+//   }
+//
+//   gameOver(snakeArr, xNext, yNext) {
+//     snakeArr.forEach(cell => {
+//       if (cell.x === xNext && cell.y === yNext) {
+//         alert('game over!');
+//       }
+//     });
+//   }
+// }
+
+
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, combineLatest, fromEvent, interval, merge, Observable, Subject} from 'rxjs';
+import {distinctUntilChanged, filter, map, mapTo, scan, startWith, switchMap, tap, withLatestFrom} from 'rxjs/operators';
+import {DIRECTIONS, Key, SNAKE_LENGTH, SNAKE_START} from './snake/snake.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovementService {
+  score$ = new BehaviorSubject(0);
+  left$: Observable<any>;
+  up$: Observable<any>;
+  down$: Observable<any>;
+  right$: Observable<any>;
+  snakeMovement$: Observable<{key: number; direction: {x: number; y: number}}>;
+
+  snake: Array<any> = [];
+  apples: Array<any> = [];
+  xSide: number;
+  ySide: number;
+  ticks$ = interval(1000);
+
+  apples$ = new BehaviorSubject([]);
+  snake$ = new Subject();
+
+  startGame$ = new Subject();
+
+  snake2$ = this.startGame$.pipe(
+    switchMap((data: {
+      snake: {
+        length: number;
+        startPoint: {}
+      }
+    }) => this.printSnakeAtTheBeggining(data.snake.length)));
+
+  move$ = this.ticks$.pipe(
+    withLatestFrom(this.snake2$, this.snakeMovement$, this.apples$),
+    map(([tick, snakeArr, movement, apples]) => {
+      this.moveSnake(tick, snakeArr, movement.direction, apples);
+    })
+  );
+
+  game$ = combineLatest(
+    this.move$,
+    this.apples$,
+    this.snake2$,
+    this.ticks$,
+    this.snakeMovement$
+  );
+
+  startGame2(left$, down$, up$, right$, x: string, y: string) {
+    this.left$ = left$;
+    this.down$ = down$;
+    this.up$ = up$;
+    this.right$ = right$;
+
+    this.snakeMovement$ = merge(
+      this.left$.pipe(mapTo({key: Key.LEFT, direction: DIRECTIONS[37]})),
+      this.up$.pipe(mapTo({key: Key.UP, direction: DIRECTIONS[38]})),
+      this.down$.pipe(mapTo({key: Key.DOWN, direction: DIRECTIONS[40]})),
+      this.right$.pipe(mapTo({key: Key.RIGHT, direction: DIRECTIONS[39]})),
+    ).pipe(
+      startWith({key: 39, direction: DIRECTIONS[39]}),
+      scan((acc, current) => {
+        if (Math.abs(acc.key - current.key) === 2) {
+          return acc;
+        } else {
+          return current;
+        }
+      }),
+      distinctUntilChanged(),
+    );
+
+    this.apples$.next([]);
+    this.score$.next(0);
+    this.xSide = +x;
+    this.ySide = +y;
+
+    this.startGame$.next({
+      snake: {
+        length: SNAKE_LENGTH,
+        startPoint: SNAKE_START
+      },
+    });
+
+    this.generateApple();
+    this.generateApple();
+  }
+
+  printSnakeAtTheBeggining(length) {
+    this.snake = [];
+    for (let i = 0; i < length; i++) {
+      this.snake.push({x: 0, y: i});
+    }
+
+    this.snake$.next(this.snake);
+    return this.snake$;
+  }
+
+  generateApple() {
+    let i = Math.floor(Math.random() * ((this.ySide - 1) + 1));
+    const j = Math.floor(Math.random() * ((this.xSide - 1) + 1));
+
+    this.apples$.getValue().forEach( apple => {
+      if (apple.x === i && apple.y === j) {
+        i = Math.floor(Math.random() * ((this.ySide - 1) + 1));
+      }
+    });
+
+    this.apples$.getValue().push({x: i, y: j});
+  }
+
+  moveSnake(tick, snakeArr, direction, apples) {
+    let yNext, xNext;
+
+    yNext = snakeArr[snakeArr.length - 1].y;
+
+    xNext = snakeArr[snakeArr.length - 1].x;
+
+    xNext += direction.x;
+    yNext += direction.y;
+
+    if (yNext > this.ySide - 1) {
+      yNext = 0;
+    }
+
+    if (yNext < 0) {
+      yNext = this.ySide - 1;
+    }
+
+    if (xNext < 0) {
+      xNext = this.xSide - 1;
+    }
+
+    if (xNext > this.xSide - 1) {
+      xNext = 0;
+    }
+
+    snakeArr.push({x: xNext, y: yNext});
+
+    let newSnakeCell = false;
+    snakeArr.forEach(snakeCoord => {
+      apples.forEach((apple, index) => {
+        if (apple.x === snakeCoord.x && apple.y === snakeCoord.y) {
+          apples.splice(index, 1);
+
+          newSnakeCell = true;
+        }
+      });
+    });
+
+    if (newSnakeCell) {
+      this.generateApple();
+      this.score$.next(this.score$.getValue() + 1);
+    }
+
+    if (!newSnakeCell) {
+      snakeArr.shift();
+    }
+
+    return snakeArr;
+  }
+}
--- src/app/snake/snake.component.ts
+++ src/app/snake/snake.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import {BehaviorSubject, combineLatest, from, fromEvent, interval, merge, Observable, Subject, Subscription} from 'rxjs';
-import {distinctUntilChanged, filter, map, mapTo, scan, startWith, switchMap, tap, withLatestFrom} from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {MovementService} from '../movement.service';
+import {fromEvent, Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
-enum Key {
+export enum Key {
   LEFT = 37,
   RIGHT = 39,
   UP = 38,
   DOWN = 40
 }
-const DIRECTIONS = {
+
+export const DIRECTIONS = {
   38: {x: -1, y: 0},
   40: {x: 1, y: 0},
   37: {x: 0, y: -1},
   39: {x: 0, y: 1}
 };
-const SNAKE_LENGTH = 3;
-const SNAKE_START = {
+export const SNAKE_LENGTH = 3;
+export const SNAKE_START = {
   x: 0,
   y: 0
 };
@@ -26,222 +28,55 @@ const SNAKE_START = {
   styleUrls: ['./snake.component.css']
 })
 export class SnakeComponent {
-  score$ = new BehaviorSubject(0);
-  left$: Observable<any> = fromEvent(document, 'keydown').pipe(
-    filter((event: KeyboardEvent) => event.keyCode === Key.LEFT)
-  );
-  up$: Observable<any> = fromEvent(document, 'keydown').pipe(
-    filter((event: KeyboardEvent) => event.keyCode === Key.UP)
-  );
+  rows;
+  left$;
+  up$;
+  down$;
+  right$;
 
-  down$: Observable<any> = fromEvent(document, 'keydown').pipe(
-    filter((event: KeyboardEvent) => event.keyCode === Key.DOWN)
-  );
-
-  right$: Observable<any> = fromEvent(document, 'keydown').pipe(
-    filter((event: KeyboardEvent) => event.keyCode === Key.RIGHT)
-  );
-  snakeMovement$: Observable<{key: number; direction: {x: number; y: number}}> = merge(
-    this.left$.pipe(mapTo({key: Key.LEFT, direction: DIRECTIONS[37]})),
-    this.up$.pipe(mapTo({key: Key.UP, direction: DIRECTIONS[38]})),
-    this.down$.pipe(mapTo({key: Key.DOWN, direction: DIRECTIONS[40]})),
-    this.right$.pipe(mapTo({key: Key.RIGHT, direction: DIRECTIONS[39]})),
-  ).pipe(
-    startWith({key: 39, direction: DIRECTIONS[39]}),
-    scan((acc, current) => {
-      if (Math.abs(acc.key - current.key) === 2) {
-        return acc;
-      } else {
-        return current;
-      }
-    }),
-    distinctUntilChanged(),
-  );
-
-  snake: Array<any> = [];
-  apples: Array<any> = [];
-  ticks$ = interval(1000);
-
-  apples$ = new BehaviorSubject([]);
-  snake$ = new Subject();
-
-  startGame$ = new Subject();
-
-  snake2$ = this.startGame$.pipe(
-    switchMap((data: {
-      snake: {
-        length: number;
-        startPoint: {}
-      }
-    }) => this.printSnakeAtTheBeggining(data.snake.length)));
-
-  move$ = this.ticks$.pipe(
-    withLatestFrom(this.snake2$, this.snakeMovement$, this.apples$),
-    map(([tick, snakeArr, movement, apples]) => {
-      this.moveSnake(tick, snakeArr, movement.direction, apples);
-    })
-  );
-
-  game$ = combineLatest(
-      this.move$,
-      this.apples$,
-      this.snake2$,
-      this.ticks$,
-      this.snakeMovement$
-    ).pipe(
-      tap( (data) => console.log(data))
-    );
-
-  startGame2() {
-    this.startGame$.next({
-      snake: {
-        length: SNAKE_LENGTH,
-        startPoint: SNAKE_START
-      },
-    });
-
-    this.generateApple();
+  constructor(private movement: MovementService) {
   }
 
-  printSnakeAtTheBeggining(length) {
-    this.snake = [];
-    for (let i = 0; i < length; i++) {
-      this.snake.push({x: 0, y: i});
-    }
+  score$ = this.movement.score$;
+  game$ = this.movement.game$;
 
-    this.snake$.next(this.snake);
-    return this.snake$;
-  }
-
-  generateApple() {
-    let i = Math.floor(Math.random() * ((10 - 1) + 1));
-    const j = Math.floor(Math.random() * ((10 - 1) + 1));
-
-    this.apples$.getValue().forEach( apple => {
-      if (apple.x === i && apple.y === j) {
-        i = Math.floor(Math.random() * ((10 - 1) + 1));
-      }
-    });
-
-    this.apples$.getValue().push({x: i, y: j});
-  }
-
-  moveSnake(tick, snakeArr, direction, apples) {
-    let yNext, xNext;
-
-    yNext = snakeArr[snakeArr.length - 1].y;
-
-    xNext = snakeArr[snakeArr.length - 1].x;
-
-    xNext += direction.x;
-    yNext += direction.y;
-
-    if (yNext > 10 - 1) {
-      yNext = 0;
-    }
-
-    if (yNext < 0) {
-      yNext = 10 - 1;
-    }
-
-    if (xNext < 0) {
-      xNext = 10 - 1;
-    }
-
-    if (xNext > 10 - 1) {
-      xNext = 0;
-    }
-
-    // snakeArr.forEach(cell => {
-    //   if (cell.x === xNext && cell.y === yNext) {
-    //     alert('game over!');
-    //   }
-    // });
-    //
-    // this.rows[xNext].children[yNext].classList.add('snake');
-    //snakeArr.shift();
-    snakeArr.push({x: xNext, y: yNext});
-
-    let newSnakeCell = false;
-    snakeArr.forEach(snakeCoord => {
-      apples.forEach((apple, index) => {
-        if (apple.x === snakeCoord.x && apple.y === snakeCoord.y) {
-         // this.rows[this.apples[index].x].children[this.apples[index].y].classList.remove('apple');
-          apples.splice(index, 1);
-
-          newSnakeCell = true;
-        }
-      });
-    });
-
-    if (newSnakeCell) {
-      this.generateApple();
-      this.score$.next(this.score$.getValue() + 1);
-    }
-
-    if (!newSnakeCell) {
-      //this.rows[snakeArr[0].x].children[snakeArr[0].y].classList.remove('snake');
-      snakeArr.shift();
-    }
-
-    return snakeArr;
-  }
-
-  // redrawMatrix(snake, apples, direction) {
-  //   this.moveSnake(snake, direction);
-  //   document.getElementsByClassName('score')[0].innerHTML = this.score$.getValue().toString();
-  // }
-  //
-  // fillMatrix(matr) {
-  //   // this.printSnakeAtTheBeggining();
-  //
-  //   this.generateApple();
-  //
-  //   this.generateApple();
-  //
-  //   return matr;
-  // }
-
-  // startGame($event) {
-  //   $event.preventDefault();
-  //   if (this.game) {
-  //     this.game.unsubscribe();
-  //     this.score$.next(0);
-  //
-  //     this.snake.forEach(cell => this.rows[cell.x].children[cell.y].classList.remove('snake'));
-  //     this.apples.forEach(cell => this.rows[cell.x].children[cell.y].classList.remove('apple'));
-  //     this.snake = [];
-  //     this.apples = [];
-  //
-  //     this.redrawMatrix(this.snake, this.apples, DIRECTIONS[39]);
-  //   }
-  //
-  //   this.game =  interval(1000)
-  //     .pipe(withLatestFrom(this.snakeState$), tap(console.log))
-  //     .subscribe(([val, state]) => {
-  //       this.redrawMatrix(this.snake, this.apples, state.direction);
-  //     });
-  //
-  //   return  this.game;
-  // }
-
-/*
-    1) stream Snake (start point, length)
-    2) stream Apples (field size)
-    3) stream field size
-    4) stream start game
-    5) stream keyBoard events
-    6) stream Game: Snake[{x: 0, y: 0}, {x: 0, y: 1}] = interval => combineLatest(startGame).pipe(withLatest())
-
- */
-
-/*  drawField($event: Event, x: string, y: string) {
+  start($event: Event, x: string, y: string) {
     $event.preventDefault();
-    this.nx = x;
-    this.ny = y;
-    this.nx$ = from(x);
-    this.ny$ = from(this.ny);
-    this.field$ = this.nx$.pipe(withLatestFrom(this.ny$));
+    // this.drawField(x, y);
+    // this.rows = document.getElementsByClassName('row');
+    this.left$ = this.getKeydownLeft(document, Key.LEFT);
+    this.up$ = this.getKeydownUp(document, Key.UP);
+    this.down$ = this.getKeydownDown(document, Key.DOWN);
+    this.right$ = this.getKeydownRight(document, Key.RIGHT);
+
+    this.movement.startGame2(this.left$, this.down$, this.up$, this.right$, x, y);
+  }
+
+  getKeydownLeft(doc, key) {
+    return fromEvent(doc, 'keydown').pipe(
+      filter((event: KeyboardEvent) => event.keyCode === key)
+    );
+  }
+
+  getKeydownUp(doc, key) {
+    return fromEvent(doc, 'keydown').pipe(
+      filter((event: KeyboardEvent) => event.keyCode === key)
+    );
+  }
+
+  getKeydownRight(doc, key) {
+    return fromEvent(doc, 'keydown').pipe(
+      filter((event: KeyboardEvent) => event.keyCode === key)
+    );
+  }
+
+  getKeydownDown(doc, key) {
+    return fromEvent(doc, 'keydown').pipe(
+      filter((event: KeyboardEvent) => event.keyCode === key)
+    );
+  }
+
+  drawField(x: string, y: string) {
     const table = document.createElement('div');
     table.classList.add('table');
     for (let i = 0; i < +x; i++) {
@@ -255,5 +90,5 @@ export class SnakeComponent {
       }
     }
     document.getElementsByTagName('app-snake')[0].appendChild(table);
-  }*/
+  }
 }
